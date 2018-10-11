edac1f5baa5bfaca92fd42ae30987c636a41c2ea
yuequan <yuequan1997@gmail.com>
2018-10-11 19:13:21 +0800

    test: Add testSoftDelete Unit Test

--- src/test/java/org/yuequan/jpa/soft/delete/SoftDeleteTest.java
+++ src/test/java/org/yuequan/jpa/soft/delete/SoftDeleteTest.java
@@ -1,15 +1,21 @@
 package org.yuequan.jpa.soft.delete;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.yuequan.jpa.soft.delete.mock.entity.User;
 import org.yuequan.jpa.soft.delete.mock.repository.UserRepository;
 import org.yuequan.jpa.soft.delete.repository.SoftDelete;
 
+import java.util.Random;
+
 @RunWith(SpringRunner.class)
+@Profile("test")
 @SpringBootTest
 public class SoftDeleteTest {
 
@@ -19,6 +25,18 @@ public class SoftDeleteTest {
     @Test
     @Transactional
     public void testSoftDelete(){
-        System.out.println(userRepository);
+        User user = getUser();
+        userRepository.save(user);
+        userRepository.delete(user);
+        User deletedUser = userRepository.findById(user.getId()).get();
+        Assert.assertNull(deletedUser);
+    }
+
+    private User getUser(){
+        User user = new User();
+        user.setUsername("Tester" + new Random().nextInt(1000));
+        user.setName("Tester");
+        user.setPassword("test");
+        return user;
     }
 }
